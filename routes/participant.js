c1698420a931a30d80fb3c3a6d037592d3c408c6
ELIDRISSI-mohamed <58290448+ELIDRISSI-mohamed@users.noreply.github.com>
2021-09-02 18:40:45 +0100

    Add files via upload

--- routes/participant.js
+++ routes/participant.js
@@ -3,11 +3,10 @@ var router = express.Router();
 const xlsx = require('xlsx');
 const multer  = require('multer');
 const path = require('path');
-const fs = require('fs')
 const jwt = require('jsonwebtoken');
+var mongodb = require('mongodb');
 
 const verifyToken = require('../middleware/verifyToken');
-const { Console } = require('console');
 
 var storage =   multer.diskStorage({
     destination: function (req, file, callback) {
@@ -51,40 +50,42 @@ function readFile(fileName){
     }
     return posts;
 }
-router.post("/add", upload.array('file'), (req, res)=>{
-    if(req.body.formationName && req.files){
-        data = readFile(req.files[0].originalname);
-        data.forEach(element => {
-            let participant = {
-                formationName: req.body.formationName,
-                nom: element.nom.toLowerCase(),
-                prenom: element.prenom.toLowerCase(),
-                email: element.email,
-                adresse: element.adresse,
-                role: "user"
-            }
+router.post("/add", verifyToken, upload.array('file'), (req, res)=>{
+    jwt.verify(req.token, process.env.TOKEN_SECRET, (err,data)=>{
+        if(err) 
+                console.log(err)
+        else{
+            console.log(data)
+            if(data.result.role == 'admin'){
+                if(req.body.formationName && req.files){
+                    data = readFile(req.files[0].originalname);
+                    data.forEach(element => {
+                        let participant = {
+                            formationName: req.body.formationName,
+                            nom: element.nom.toLowerCase(),
+                            prenom: element.prenom.toLowerCase(),
+                            email: element.email,
+                            adresse: element.adresse,
+                            role: "user"
+                        }
 
-            dbo.collection('participants').insertOne(participant, (err, result)=> {
-                if (err) 
-                    res.send(err);
-                else{
+                        dbo.collection('participants').insertOne(participant, (err, result)=> {
+                            if (err) 
+                                res.send(err);
+                            else{
+                            }
+                            
+                        })
+                    });
+                    res.status(200).send({MESSAGE: "ADD_SUCCES"});
+                } else{
+                    res.send({ERROR: "BODY_ERREUR"})
                 }
-                
-            })
-        });
-        res.status(200).send({MESSAGE: "ADD_SUCCES"});
-    } else{
-        res.send({ERROR: "BODY_ERREUR"})
-    }
-})
-
-router.post("/addCertificate", upload.single('img'), (req, res)=>{
-    if(req.file){
-            res.send("ok")
-        
-    } else{
-        res.send({ERROR: "BODY_ERROR"})
-    }
+            } else{
+                res.json({"ERROR" : "NO_ACCESS"});
+            }
+        }
+    })
 })
 
 router.get("/findByFormation/:formationName", (req, res)=>{
@@ -109,15 +110,16 @@ router.get("/findFormations", (req, res)=>{
     })
 })
 
-router.delete("/delete", (req, res)=>{
-    dbo.collection("participants").deleteOne({"nom": req.body.nom, "prenom": req.body.prenom, "formationName": req.body.formationName}, (err, result)=> {
-        if (err) 
-            throw err;
-        else{
-            if(result.deletedCount != 0)  res.status(200).send({MESSAGE: "DELETE_SUCCES"});
-            else res.send({ERROR: "N'existe pas"})
-        }
-    })
+router.delete("/delete/:id", (req, res)=>{
+        dbo.collection("participants").deleteOne({_id: new mongodb.ObjectId(req.params.id)}, (err, result)=> {
+            if (err) 
+                throw err;
+            else{
+                console.log(result)
+                if(result.deletedCount != 0)  res.status(200).send({MESSAGE: "DELETE_SUCCES"});
+                else res.send({ERROR: "N'existe pas"})
+            }
+        })
 })
 
 router.post("/findFormationsParticiper", (req, res)=>{
